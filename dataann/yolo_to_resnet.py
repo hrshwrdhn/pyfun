0cda87eb0527b697fdefadd8e9b6f21f6c8df241
HARSH WARDHAN   SINGH <hrshwrdhn@gmail.com>
2021-02-01 18:54:12 +0530

    Update yolo_to_resnet.py

--- dataann/yolo_to_resnet.py
+++ dataann/yolo_to_resnet.py
@@ -72,3 +72,28 @@ for imgname in os.listdir(imagefolder):
         f.write(txtline)
         #print(txtline)
 f.close()
+
+
+
+#########
+#check non annotated images
+count1 = 0
+for imgname in os.listdir(imagefolder):
+    count += 1
+
+    #if count == 10:
+    #    break
+
+    imgn = imgname.split('.')[0]
+    image1 = cv2.imread(imagefolder + imgname)
+    bbox1 = readboxyolo5(labelfolder, imgn, image1)
+    #print(bbox1)
+    bbx = bbox1.shape
+    if bbx[0] == 0:
+        print(imgname)
+        os.remove(imagefolder + imgname)
+        os.remove(labelfolder + imgn + '.txt')
+        count1 +=1
+print(count1)
+    
+    
